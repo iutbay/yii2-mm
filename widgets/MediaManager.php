81903fe1765d04d91b0f7766804a0a352488bdd0
Kevin LEVRON <kevin.levron@gmail.com>
2017-04-30 16:36:27 +0200

    wip

---
+++ widgets/MediaManager.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace iutbay\yii2\mm\widgets;
+
+use Yii;
+use yii\helpers\Json;
+
+use iutbay\yii2\mm\widgets\MediaManagerAsset;
+
+class MediaManager extends \yii\base\Widget
+{
+
+    /**
+     * MM options
+     * @var array
+     */
+    public $clientOptions = [];
+
+    /**
+     * @inheritdoc
+     */
+    public function run()
+    {
+        $this->registerClientScript();
+    }
+
+    /**
+     * Register js
+     */
+    public function registerClientScript()
+    {
+        $view = $this->getView();
+        MediaManagerAsset::register($view);
+
+        $options = $this->clientOptions;
+        $id = $this->getId();
+        $options['el'] = "#$id";
+        $options = Json::encode($options);
+        $view->registerJs("new MM($options);");
+    }
+
+}
---
+++ widgets/MediaManagerAsset.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace iutbay\yii2\mm\widgets;
+
+use yii\web\AssetBundle;
+
+class MediaManagerAsset extends AssetBundle
+{
+
+    public $basePath = '@vendor/iutbay/yii2-mm/assets/mm';
+    public $css = [
+        'mm.min.css',
+    ];
+    public $js = [
+        'mm.min.js',
+    ];
+    public $depends = [
+    ];
+    public $publishOptions = [
+        'forceCopy' => YII_DEBUG,
+    ];
+
+}
